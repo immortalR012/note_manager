25f64e174d153c8acbda8f6d5f67a02117a8ecd8
immortalR012 <r.bessmertny@yandex.ru>
2024-12-15 23:40:14 +0300

    15.12

---
+++ date_changer.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+created_date=(input("ведите дату для создания заметки"))
+issue_date=(input("ведите дату истечение срока заметки"))
+created_date_obj = datetime.strptime(created_date, "%d %m %Y")
+issue_date_obj = datetime.strptime(issue_date, "%d %m %Y")
+
+print(created_date_obj.day,issue_date_obj.day)
+print(created_date_obj.day,issue_date_obj.month)
+
+
