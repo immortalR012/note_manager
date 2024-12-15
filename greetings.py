6a5818000c8dce70261942c0df1e725f1ba15c51
immortalR012 <r.bessmertny@yandex.ru>
2024-12-15 12:11:33 +0300

    15.12

---
+++ greetings.py
@@ -0,0 +1,7 @@
+username= input("ведите ваше имя")
+title=input("ведите заголовок" )
+content=input("ведите текст")
+status="статус заметки"
+created_date=input("ведите дату")
+issue_date=input("ведите дату завешения")
+print(username)
