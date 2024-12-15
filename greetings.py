9542287a58277b7be2d7ce68b0235686c5b55dda
immortalR012 <r.bessmertny@yandex.ru>
2024-12-15 17:05:50 +0300

    15.12

--- greetings.py
+++ greetings.py
@@ -1,7 +1,2 @@
 username= input("ведите ваше имя")
-title=input("ведите заголовок" )
-content=input("ведите текст")
-status="статус заметки"
-created_date=input("ведите дату")
-issue_date=input("ведите дату завешения")
 print(username)
