e41d189432f98c04d0e40cb6be24bcf699e7b133
immortalR012 <r.bessmertny@yandex.ru>
2025-01-02 21:55:52 +0300

    02 01 2025

---
+++ create_note_function.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+today = datetime.now()
+name = input('ведите имя')
+title = input('ведите заголовок')
+content = input('напишите заметку')
+issue_date = (input('ведите дату число месяц истечение срока заметки'))
+issue_date_obj = datetime.strptime(issue_date, "%d %m %Y")
+user_data = {'name': name,
+             'title': title,
+             'content': content,
+             'issue_date': issue_date,
+             }
+try:
+    issue_date_obj = datetime.strptime(issue_date, '%d %m %Y').date()
+except ValueError as e:
+    print('Ошибка при вводе даты: {e}')
+else:
+    user_data = {
+        'name': name,
+        'title': title,
+        'content': content,
+        'issue_date': issue_date_obj
+    }
+print(today.date())
+print(user_data)
+
