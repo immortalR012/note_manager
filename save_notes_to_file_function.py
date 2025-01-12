8ad247d06b5bf9dae01cde201491bc7c33fc23be
immortalR012 <r.bessmertny@yandex.ru>
2025-01-12 21:54:49 +0300

    12.01.2025

--- save_notes_to_file_function.py
+++ save_notes_to_file_function.py
@@ -1,32 +1,48 @@
-file = open(' filename.txt','w', encoding='utf-8')
-from datetime import datetime
+import datetime
 
-today = datetime.now().date()
+def get_user_input():
+    today = datetime.datetime.now().date()
+    name = input('Введите имя: ')
+    title = input('Введите заголовок: ')
+    content = input('Напишите заметку: ')
+    issue_date_str = input('Введите дату истечения срока заметки (число месяц год): ')
 
-name = input('Введите имя:')
-title = input('Введите заголовок:')
-content = input('Напишите заметку:')
-issue_date = input('Введите дату истечения срока заметки (число месяц год): ')
+    try:
+        issue_date_obj = datetime.datetime.strptime(issue_date_str, '%d %m %Y').date()
+    except ValueError as e:
+        print(f'Ошибка при вводе даты: {e}')
+        return None
 
-try:
-    issue_date_obj = datetime.strptime(issue_date, '%d %m %Y').date()
-except ValueError as e:
-    print(f'Ошибка при вводе даты: {e}')
-    exit()
-notes = {
-    'name': name,
-    'title': title,
-    'content': content,
-    'issue_date': issue_date_obj
-}
-try:
-    with open('filename.txt','w',encoding='utf-8') as file:
-        file.write(f"{notes}\n")
-    def load_notes_from_file():
-        with open('filename.txt', 'r', encoding='utf-8'):
-            print(file.read())
-except FileNotFoundError:
-    print('файл не найден')
-except PermissionError:
-    print('нет доступа к файлу')
-file.close()
+    note = {
+        'name': name,
+        'title': title,
+        'content': content,
+        'issue_date': issue_date_obj
+    }
+    return note
+
+
+def save_note(note):
+    try:
+        with open('filename.txt', 'a', encoding='utf-8') as file:
+            file.write(f"{note}\n")
+    except FileNotFoundError:
+        print('Файл не найден.')
+    except PermissionError:
+        print('Нет доступа к файлу.')
+
+
+def load_notes_from_file():
+    try:
+        with open('filename.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line())
+    except FileNotFoundError:
+        print('Файл не найден.')
+
+
+if __name__ == "__main__":
+    note = get_user_input()
+    if note:
+        save_note(note)
+    load_notes_from_file()
